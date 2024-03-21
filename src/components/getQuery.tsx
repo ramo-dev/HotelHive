f9524475ec853e6bafe110bdc17ea08dd9850c24
ramo-dev <annuar.dev@gmail.com>
2024-03-22 00:13:55 +0300

    HotelHive

--- src/components/getQuery.tsx
+++ src/components/getQuery.tsx
@@ -1,7 +1,8 @@
-const getQuery =  async (search:any) => {
-    const res = await fetch(`https://hotelhive-backend.onrender.com/api/search/${search}`)
-    const data = await res.json()
-    return data
-}
+const getQuery = async (search: any) => {
+  const url = import.meta.env.VITE_BACKEND_URL;
+  const res = await fetch(url + search);
+  const data = await res.json();
+  return data;
+};
 
 export default getQuery;
