5fa3be144f703139fbf5cec022ca0b0a58262dcf
ramo-dev <annuar.dev@gmail.com>
2024-03-21 15:27:45 +0300

    HotelHive

--- src/components/Home/HomeData.tsx
+++ src/components/Home/HomeData.tsx
@@ -1,44 +1,61 @@
-import Hero from "../Hero/HeroData";
-import Navbar from "../navbar/Navigation";
+import { useState, useEffect } from "react";
+import {
+  Card,
+  Typography,
+  Flex,
+  Rate,
+  Space,
+  Button,
+  Skeleton,
+  Layout,
+} from "antd";
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
-import getQuery from "../getQuery";
-import "./homeStyles.css";
-import {RandomLocation} from "./Locations";
-import { Card, Layout, Typography, Flex, Rate, Space, Button } from "antd";
-import { Content } from "antd/es/layout/layout";
-import SiderContent from "../Sider/SiderContent";
 import { SearchOutlined } from "@ant-design/icons";
-const { Sider } = Layout;
+import Navbar from "../navbar/Navigation";
+import Hero from "../Hero/HeroData";
+import getQuery from "../getQuery";
+import { RandomLocation } from "./Locations";
+import SiderContent from "../Sider/SiderContent";
+import "./homeStyles.css";
 
-
+const { Content, Sider } = Layout;
 
 const HomeData = () => {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [location, setLocation] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchLoading, setSearchLoading] = useState(false);
 
-  async function getLocations(){
-    const resp = await getQuery(query)
-    const data = await resp
-    console.log(data)
-    setLocation(data)
-      
+  async function getLocations() {
+    setSearchLoading(true);
+    const resp = await getQuery(query);
+    const data = await resp;
+    setLocation(data);
+    setSearchLoading(false);
   }
-
-  function handleSubmit(e:any){
-    e.preventDefault();
-    getLocations();
-  }
-
-useEffect(() => {
-  async function getHotelsOnLoad(){
+  async function getHotelsOnLoad() {
     const getRandomLocation = RandomLocation();
     const arrayOfFetchedLocations = await getQuery(getRandomLocation);
     setLocation(arrayOfFetchedLocations);
+    setLoading(false);
   }
-  getHotelsOnLoad()
-},[])
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      getHotelsOnLoad();
+    }, 2000); // Adjust timeout duration as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (query === "") {
+      getHotelsOnLoad();
+    } else {
+      getLocations();
+    }
+  }
 
   return (
     <>
@@ -48,7 +65,6 @@ useEffect(() => {
         style={{ marginTop: "15rem", width: "100%", borderRadius: "30px" }}
         className="mainSection"
       >
-        {" "}
         <Sider
           className="sider"
           theme="light"
@@ -58,18 +74,22 @@ useEffect(() => {
           style={{ borderTopLeftRadius: "30px" }}
           width={200}
         >
-          {/*  */}
           <SiderContent />
         </Sider>
         <Content>
           <Flex
             align="center"
             justify="space-around"
-            gap="large"
+            gap="small"
             className="top"
+            wrap="wrap"
           >
             <Typography.Title level={2}>
-              {location && <>{location.length} hotels Found</>}
+              {location.length > 0 ? (
+                <>{location.length} hotels Found</>
+              ) : (
+                <>Welcome to HotelHive</>
+              )}
             </Typography.Title>
             <Flex align="center" gap="large">
               <form className="destinationSearchMain" onSubmit={handleSubmit}>
@@ -85,7 +105,14 @@ useEffect(() => {
             </Flex>
           </Flex>
           <Flex wrap="wrap" gap="middle" style={{ padding: "1rem 0 0 1rem" }}>
-            {location ? (
+            {loading || searchLoading ? (
+              // Display Skeleton while loading
+              <>
+                <Skeleton active />
+                <Skeleton active />
+                <Skeleton active />
+              </>
+            ) : location && location.length > 0 ? (
               <>
                 {location.map((hotel, index) => (
                   <Card
@@ -95,60 +122,87 @@ useEffect(() => {
                     className="hotelCard"
                   >
                     <Flex wrap="wrap" gap="middle">
-                      <img
-                        src={hotel.thumbnails[0]}
-                        alt={hotel.title}
-                        style={{ width: "300px" }}
-                      />
+                      <Skeleton loading={!hotel.thumbnails} active>
+                        <img
+                          src={hotel.thumbnails[0]}
+                          alt={
+                            hotel.thumbnails
+                              ? hotel.title
+                              : `photo for ${hotel.title} not found`
+                          }
+                          style={{ width: "300px" }}
+                        />
+                      </Skeleton>
                       <Flex
                         vertical
                         style={{ maxWidth: "240px" }}
                         gap="small"
                         align="start"
                       >
-                        <Typography.Title level={4}>
-                          {hotel.title}
-                        </Typography.Title>
-                        <Typography.Title level={5}>
-                          {hotel.location}
-                        </Typography.Title>
-                        <Typography.Text strong>{hotel.price}</Typography.Text>
-                        <Flex>
-                          <Rate
-                            allowHalf
-                            disabled
-                            defaultValue={hotel.rating}
-                          />
-                          <Typography.Text>({hotel.rating})</Typography.Text>
-                        </Flex>
-                        <Typography.Text>
-                          Reviews: ({hotel.reviews})
-                        </Typography.Text>
-                        <Space />
-                        <Flex gap="small">
-                          <Button>
-                            <Link to={hotel.link}>Hotel Details</Link>
-                          </Button>
-                          <Button
-                            style={{
-                              background: "var(--primary)",
-                              color: "var(--textWhite)",
-                            }}
-                          >
-                            <Link to={hotel.link}>Add To Favourites</Link>
-                          </Button>
-                        </Flex>
+                        <Card.Meta
+                          title={
+                            <Typography.Title
+                              level={4}
+                              style={{
+                                maxWidth: "200px",
+                                whiteSpace: "wrap",
+                              }}
+                            >
+                              {hotel.title}
+                            </Typography.Title>
+                          }
+                          description={
+                            <>
+                              <Typography.Title
+                                level={5}
+                                style={{ marginTop: "-1rem" }}
+                              >
+                                <span style={{fontWeight : '500'}}> Location :</span> {hotel.location}
+                              </Typography.Title>
+                              <Typography.Text strong>
+                                {hotel.price}
+                              </Typography.Text>
+                              <Flex>
+                                <Rate
+                                  allowHalf
+                                  disabled
+                                  defaultValue={
+                                    hotel.rating > 0 ? hotel.rating : 0
+                                  }
+                                />
+                                <Typography.Text>
+                                  ({hotel.rating})
+                                </Typography.Text>
+                              </Flex>
+                              <Typography.Text>
+                                Reviews: (
+                                {hotel.reviews > 0 ? hotel.reviews : 0})
+                              </Typography.Text>
+                              <Space />
+                              <Flex gap="small">
+                                <Button>
+                                  <Link to={hotel.link}>Hotel Details</Link>
+                                </Button>
+                                <Button
+                                  style={{
+                                    background: "var(--primary)",
+                                    color: "var(--textWhite)",
+                                  }}
+                                >
+                                  <Link to={hotel.link}>Add To Favourites</Link>
+                                </Button>
+                              </Flex>
+                            </>
+                          }
+                        />
                       </Flex>
                     </Flex>
                   </Card>
                 ))}
               </>
             ) : (
-              <>
-                <Typography.Title level={2}>No Hotels Found</Typography.Title>
-              </>
+              <Typography.Title level={2}>No Hotels Found</Typography.Title>
             )}
-            )
           </Flex>
         </Content>
       </Layout>
--- src/components/Sider/SiderContent.tsx
+++ src/components/Sider/SiderContent.tsx
@@ -1,12 +1,65 @@
-import { Flex, Typography, Checkbox } from "antd";
+import {
+  Flex,
+  Typography,
+  Slider,
+  Rate,
+  Switch,
+  Select,
+  Space,
+  InputNumber,
+} from "antd";
+const { Option } = Select;
 
 const SiderContent = () => {
   return (
     <>
-      <Flex vertical align="center">
-        <Typography.Title level={4}>Category : </Typography.Title>
-        <Typography.Text>Board Basis</Typography.Text>
-        <Checkbox>Hotel</Checkbox>
+      <Flex vertical align="center" className="Ratings">
+        <Typography.Title level={4}>Sort by:</Typography.Title>
+        <Flex vertical gap="2rem" align="center">
+          <Select
+            defaultValue="Popularity"
+            style={{ width: 130, marginTop: "1rem" }}
+            allowClear
+            options={[
+              { value: "Price", label: "Popularity" },
+              { value: "A-Z", label: "A-Z" },
+            ]}
+          />
+
+          <Flex vertical align="center">
+            <Typography.Text>Reviews</Typography.Text>
+            <Rate
+              allowHalf
+              defaultValue={0}
+              style={{
+                border: "1px solid lightgray",
+                padding: ".6rem .8rem",
+                borderRadius: "8px",
+              }}
+            />
+          </Flex>
+          <Flex vertical align="center">
+            <Typography.Text>Price Range</Typography.Text>
+            <InputNumber
+              addonBefore="+"
+              addonAfter="ksh"
+              defaultValue={100}
+              style={{ maxWidth: "10rem" }}
+            />
+          </Flex>
+
+          <Flex vertical align="center">
+            <Typography.Text>Amenities</Typography.Text>
+            <Switch defaultChecked />
+          </Flex>
+          <Flex vertical align="center">
+            <Typography.Text>Special Offers</Typography.Text>
+            <Switch defaultChecked />
+          </Flex>
+          <Flex vertical align="center">
+            <Typography.Text>Brand</Typography.Text>
+          </Flex>
+        </Flex>
       </Flex>
     </>
   );
